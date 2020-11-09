8929daa2182314b940b84f0c182bbf48c3846f08
mnpenchev <mnpenchev@gmail.com>
2020-11-09 13:21:06 +0000

    base logic dnd

--- dndgame/urls.py
+++ dndgame/urls.py
@@ -3,4 +3,5 @@ from dndgame import views
 
 urlpatterns = [
     path('dndgame/', views.dndgame, name='dndgame'),
+    path('choose_hero/', views.choose_hero, name='choose_hero'),
 ]
--- dndgame/views.py
+++ dndgame/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
+fighter = {'name': 'Korgan', 'attack_num': 3, 'attack_dice': 20, 'max_health': 250, 'health': 250, 'AC': 15, 'init': 16,
+           'experience': 0, 'gold': 0, 'level': 1}
+mage = {'name': 'Edwin', 'attack_num': 2, 'attack_dice': 15, 'max_health': 200, 'health': 200, 'AC': 12, 'init': 10,
+        'experience': 0, 'gold': 0, 'level': 1}
+cleric = {'name': 'Viconia', 'attack_num': 1, 'attack_dice': 15, 'max_health': 120, 'health': 120, 'AC': 12, 'init': 11,
+          'experience': 0, 'gold': 0, 'level': 1}
+rogue = {'name': 'Yoshimo', 'attack_num': 4, 'attack_dice': 15, 'max_health': 100, 'health': 100, 'AC': 14, 'init': 18,
+         'experience': 0, 'gold': 0, 'level': 1}
+
 
 def dndgame(request):
     return render(request, 'dnd_game.html')
+
+
+def choose_hero(request):
+    hero = int(request.GET['bb'])
+    if hero == 1:
+        return render(request, 'dnd_game.html', {'hero': fighter})
+
+    if hero == 2:
+        return render(request, 'dnd_game.html', {'hero': mage})
+
+    if hero == 3:
+        return render(request, 'dnd_game.html', {'hero': cleric})
+
+    if hero == 4:
+        return render(request, 'dnd_game.html', {'hero': rogue})
+
+    #return render(request, 'dnd_game.html', {'hero': fighter})
